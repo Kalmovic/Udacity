cfa543ab5b0ee539a9eff34b62684f30ba186623
vagrant <vagrant@vagrant.vm>
2019-01-16 22:42:38 +0000

    adding Cat Clicker

---
+++ Project_Cat_Clicker_Premium/js/main.js
@@ -0,0 +1,32 @@
+var cats = [
+    {name: 'Fluffy', count: 0, image: 'images/cat_picture1.jpg'},
+    {name: 'Robert', count: 0, image: 'images/cat_picture2.jpg'}
+];
+
+var $catname = $('.catname');
+var $clicks = $('#clicks');
+var $pic = $('#pic');
+var $page = $('#page');
+var $aside = $('#aside');
+var $selectedCat = $('.selectedCat');
+
+for (var i = 0; i < cats.length; i++){
+    var cat = cats[i];
+    var node = document.createElement("h4");
+    node.setAttribute("type", "text");
+    var cattext = document.createTextNode(cat.name);
+    node.appendChild(cattext);
+    document.querySelector('.catname').appendChild(node);
+    var imgcat = document.querySelector("#image");
+    
+    node.addEventListener('click', (function(cattextCopy){
+        return function() {
+            $selectedCat.text(cattextCopy.nodeValue);
+            $clicks.text(parseInt(cat.count));
+            imgcat.setAttribute("src", this.cat.image);
+            document.img.appendChild(imgcat);
+            //alert(cattextCopy.nodeValue);
+        };
+    })(cattext));
+
+}
