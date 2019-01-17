74fe03ef1ee4111403af57db0c46b42abaadc6d1
vagrant <vagrant@vagrant.vm>
2019-01-17 01:41:10 +0000

    need to fix de counting

--- Project_Cat_Clicker_Premium/js/main.js
+++ Project_Cat_Clicker_Premium/js/main.js
@@ -1,32 +1,31 @@
 var cats = [
     {name: 'Fluffy', count: 0, image: 'images/cat_picture1.jpg'},
-    {name: 'Robert', count: 0, image: 'images/cat_picture2.jpg'}
+    {name: 'Robert', count: 0, image: 'images/cat_picture2.jpg'},
+    {name: 'Tiger', count: 0, image: 'images/cat_picture3.jpg'},
+    {name: 'Oscar', count: 0, image: 'images/cat_picture4.jpg'},
+    {name: 'Max', count: 0, image: 'images/cat_picture5.jpg'}
 ];
 
 var $catname = $('.catname');
 var $clicks = $('#clicks');
-var $pic = $('#pic');
+var $pic = $('#image');
 var $page = $('#page');
 var $aside = $('#aside');
 var $selectedCat = $('.selectedCat');
 
 for (var i = 0; i < cats.length; i++){
     var cat = cats[i];
-    var node = document.createElement("h4");
-    node.setAttribute("type", "text");
-    var cattext = document.createTextNode(cat.name);
-    node.appendChild(cattext);
-    document.querySelector('.catname').appendChild(node);
-    var imgcat = document.querySelector("#image");
-    
-    node.addEventListener('click', (function(cattextCopy){
+    $catname.append('<li id="cat'+ i +'">'+ cat.name +'</li>');
+    $('#cat'+i).click((function(catCopy){
         return function() {
-            $selectedCat.text(cattextCopy.nodeValue);
-            $clicks.text(parseInt(cat.count));
-            imgcat.setAttribute("src", cat.image);
-            document.img.appendChild(imgcat);
-            //alert(cattextCopy.nodeValue);
+            $selectedCat.text(catCopy.name);
+            $clicks.text(parseInt(catCopy.count));
+            $pic.attr("src", catCopy.image);
+            $pic.click(function(e){
+                catCopy.count = $clicks.text();
+                $clicks.text(parseInt(catCopy.count)+1);
+                return catCopy.count;
+            });
         };
-    })(cattext));
-
+    })(cat));
 }
